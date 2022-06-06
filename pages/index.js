97c20fa5d51cdd31590c2ae6a285dd54656ea654
Karen Zoroglyan <karen.zoroglyan@mail.ru>
2022-06-06 14:51:45 +0400

    add Search

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Avatar from "../components/Avatar";
-import { ViewGridIcon } from "@heroicons/react/solid";
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -33,6 +35,18 @@ export default function Home() {
       </header>
 
       {/* Body */}
+      <form className="flex flex-col items-center mt-44 flex-grow">
+        <Image
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={100}
+          width={300}
+        />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+      </form>
       {/* Footer */}
     </div>
   );
